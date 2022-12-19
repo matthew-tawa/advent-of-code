733b743c0094650725cc1c94ec07c73152c73fda
matthew tawa <matthew@tawacentral.net>
2022-12-18 21:17:07 -0500

    day 7 part 1 and 2 working

--- 2022/7/aoc_day7.py
+++ 2022/7/aoc_day7.py
@@ -1,17 +1,25 @@
 import re
 
+input_file = '7\\input.txt'
+#input_file = '7\\terminal_output.txt'
 
 
-#f = open('7\\terminal_output.txt', 'r')
-f = open('7\input.txt', 'r')
+f = open(input_file, 'r')
 terminal_output = ""
 
-sum_of_dir_sizes = 0
+sum_of_dir_sizes = 0    # holds sum of all dirs <= 100000
+total_space_used = 0    # holds total sum of files
+total_drive_space = 70000000    # total drive space
+update_size = 30000000  # space required for update
+min_space_to_clear = 0  # smallest amount of data to clear to be able to update
+dir_with_min_size = total_drive_space   # size of smallest dir to delete to make enough room
+
 cmd_format = "^\$ (?P<cmd>..) *(?P<arg>.*)"
 result_format = "(?P<ftype>.*) (?P<name>.*)"
 
 
 # adds the current dirs size to sum_of_dir_sizes if the dirs size is <=100000
+# and finds the min dir size to remove to be able to update
 def add_all_dir_sizes() -> int:
     current_dir_size = 0
 
@@ -29,7 +37,7 @@ def add_all_dir_sizes() -> int:
         match code_word:
             case "cd":
                 if (cmd_matches.group("arg") == ".." or cmd_matches.group("arg") == "\n"):
-                    __add_to_sum_of_dir_sizes(current_dir_size)
+                    __process_dir_size(current_dir_size)
                     return current_dir_size
                 else: # we are going into a subdirectory
                     current_dir_size += add_all_dir_sizes()
@@ -40,22 +48,45 @@ def add_all_dir_sizes() -> int:
                 pass # nothing to do
 
             case "end_of_file":
-                __add_to_sum_of_dir_sizes(current_dir_size)
+                __process_dir_size(current_dir_size)
                 return current_dir_size
 
             case _: # the code_word is a file size
                 current_dir_size += int(code_word)
 
 
-def __add_to_sum_of_dir_sizes(dir_size: int):
-    global sum_of_dir_sizes
+def __process_dir_size(dir_size: int):
+    global sum_of_dir_sizes, dir_with_min_size
+
+    dir_with_min_size = dir_size if (dir_size >= min_space_to_clear and dir_size <= dir_with_min_size) else dir_with_min_size
+
     sum_of_dir_sizes += dir_size if (dir_size <= 100000) else 0
 
 
 
 
 def main():
+    global f, total_space_used, min_space_to_clear
 
+    # finding the total amount of space used
+    input = f.readline()
+
+    alive = True
+    while alive:
+        size_match = re.search("(?P<fsize>\d+) .*", input)
+
+        if (size_match):
+            total_space_used += int(size_match.group("fsize"))
+        
+        input = f.readline()
+        alive = input != ""
+
+    f.close()
+
+    min_space_to_clear = total_space_used - (total_drive_space - update_size)
+
+    # going through all the directoris one by one to figure out the min size we can delete
+    f = open(input_file, 'r')
     alive = True
     while alive:
         
@@ -66,8 +97,12 @@ def main():
 
     f.close()
 
-    print("sum: " + str(sum_of_dir_sizes))
-
+    print("*************************************************************")
+    print("sum of dirs less than 100000: " + str(sum_of_dir_sizes))
+    print("*************************************************************")
+    print("          min space to clear: " + str(min_space_to_clear))
+    print("          min size to delete: " + str(dir_with_min_size))
+    print("*************************************************************")
 
 
 
