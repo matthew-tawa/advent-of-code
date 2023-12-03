4d9700da310bfe6ef207df5682892385f05c7f9f
matthew tawa <matthew@tawacentral.net>
2023-12-02 20:25:02 -0500

    day2 part2 working! also realized there was an error in my regex, so added a ? for the final group in part 1. doesnt actually affect the results for part1

--- 2023/2/aoc_day2.py
+++ 2023/2/aoc_day2.py
@@ -7,6 +7,8 @@ num_tokens = {
     'blue' : 14,
 }
 
+
+
 def main1():
     # ***** IMPORT INPUT FILE *****
     # obtaining the proper relative path
@@ -22,7 +24,7 @@ def main1():
 
     possible_game_id_sum = 0
 
-    pattern_text = r" (?P<num>\d+) (?P<color>[a-z]+)(?P<delim>,|;|\n)"
+    pattern_text = r" (?P<num>\d+) (?P<color>[a-z]+)(?P<delim>(,|;|\n)?)"
     pattern = re.compile(pattern_text)
 
     for game in game_log:
@@ -43,12 +45,49 @@ def main1():
             
         if game_possible:
             possible_game_id_sum += id
-
-
-
+    
     print("sum of possible games: " + str(possible_game_id_sum))
+
+
+def main2():
+    # ***** IMPORT INPUT FILE *****
+    # obtaining the proper relative path
+    script_location = Path(__file__).absolute().parent
+    file_name = 'record.txt'
+    file_name = 'input.txt'
+    myfile = script_location / file_name
+
+    # read the file into a list, split by the newline characters
+    # note:  read_text() automatically closes file when done 
+    game_log = myfile.read_text().split('\n')
+    # *****************************
+
+    game_power_sum = 0
+
+    pattern_text = r" (?P<num>\d+) (?P<color>[a-z]+)(?P<delim>(,|;|\n)?)"
+    pattern = re.compile(pattern_text)
+
+    for game in game_log:
+        matches = pattern.findall(game)
+
+        pull = 0
+        min_tokens = {
+            'red'  : 0,
+            'green': 0,
+            'blue' : 0,
+        }
+        while (pull < len(matches)):       
+            min_tokens[matches[pull][1]] = max(min_tokens[matches[pull][1]], int(matches[pull][0]))
+
+            pull += 1
+
+        mysum = min_tokens['red'] * min_tokens['green'] * min_tokens['blue']
+        game_power_sum += mysum
+
+    print("sum of the power of each set: " + str(game_power_sum))
 
 
 
 main1()
+main2()
 
